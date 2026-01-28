994fee0b90641ebffd531fc1097103eb62da1471
Bardia <116668283+Bvrdiaa@users.noreply.github.com>
2026-01-28 09:44:00 +0100

    Add files via upload
    La fonction "generer_case_noire" qui modifie la grille en créant une case noire et retourne les coordonnées de la case créée.

---
+++ generer_case_noire.py
@@ -0,0 +1,23 @@
+from random import*
+
+
+def generer_case_noire(grille):
+    """ renvoie les coordonnées de la case noire (tuple de deux valeurs)
+    et modifie la grille
+    Entrée : La grille 
+    Sortie : Coordonnées de la case noire"""
+    
+    n = len(grille)
+    x = random.randint(0, n-1)
+    y = random.randint(0, n-1)
+    
+    grille[y,x] = -1 # Case noire créée
+    
+    return x, y
+    
+
+
+
+
+
+    
