b5785a199094bb687896506843df8af78daf1af2
GMONNET-prof <gregory.monnet69@gmail.com>
2026-01-30 11:53:10 +0100

    plus clair

--- resoudre_tableau.py
+++ resoudre_tableau.py
@@ -1,11 +1,7 @@
 def resoudre_tableau(grille,x,y,delta):
     orientation=trouve_case_occupee(grille, x, y, delta)
-    print(orientation, x, x+delta, y,y+delta)
-   
-    if delta ==2:
-        
+    if delta ==2:  
         maj_tableau(grille,x,y,orientation)
-        
     else:
         delta=delta//2
         maj_tableau(grille,x+delta-1,y+delta-1,orientation)
