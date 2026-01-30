1d67ba03adc6ff22e56db2ceae806223d09dc597
GMONNET-prof <gregory.monnet69@gmail.com>
2026-01-30 11:39:23 +0100

    Fix boundary conditions in trouve_case_occupee

--- trouve_case_occupee.py
+++ trouve_case_occupee.py
@@ -2,11 +2,11 @@ def trouve_case_occupee(grille, x, y, delta):
     for i in range(x,x+delta):
         for j in range(y,y+delta):
             if grille[i][j]!=0:
-                if i < delta//2 and j < delta//2:
+                if i < x+delta//2 and j < y+delta//2:
                     return "BD"
-                elif i >= delta//2 and j >= delta//2:
+                elif i >= x+delta//2 and j >=y+ delta//2:
                     return"HG"
-                elif i < delta//2 and j >= delta//2: 
+                elif i < x+delta//2 and j >= y+delta//2: 
                     return"BG"
                 else:
                     return"HD"
