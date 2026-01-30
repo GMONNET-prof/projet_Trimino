95d6183412d2d263be80844fb2f4f5325c42a207
GMONNET-prof <gregory.monnet69@gmail.com>
2026-01-30 10:47:45 +0100

    correction de la creation_tableau

--- creation_tableau.py
+++ creation_tableau.py
@@ -1,7 +1,9 @@
 def creation_tableau(taille):
     tab = []
-    tab_tmp=[0]*taille
-    print(tab_tmp)
+    
+    
     for i in range (taille):
+        tab_tmp=[0]*taille
         tab.append(tab_tmp)
     return tab   
+
